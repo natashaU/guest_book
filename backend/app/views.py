daac6acee09a65d9581828b7bb46e42b530465ed
Natasha Umer <natasha.umer2@gmail.com>
2016-05-18 22:58:50 -0400

    added custom index page

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+  landing_page_content = "<html><body>hello world welcome to my page!</body></html>"
+  my_response = HttpResponse(landing_page_content)
+  return my_response
