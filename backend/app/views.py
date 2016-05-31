c7495dc96a4e0ea929ae7b5355a5f5527ac0146d
Natasha Umer <natasha.umer2@gmail.com>
2016-05-30 21:45:38 -0400

    added updated template stuff

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  landing_page_content = "<html><body>hello world welcome to my page!</body></html>"
-  my_response = HttpResponse(landing_page_content)
-  return my_response
+  foo = request.GET.get('first_name')
+  bar = request.GET.get('last_name')
+  context = {
+    "baz": foo,
+    "bat": bar
+  }
+  return render(request, "index.html", context)
