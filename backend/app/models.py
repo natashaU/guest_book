040e24a2631d821034f9861ce219ed1d057588c4
Natasha Umer <natasha.umer2@gmail.com>
2016-06-07 22:54:22 -0400

    Guest model added

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Guest(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
