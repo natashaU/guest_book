113836f57f41e17cc82b0ec0b4926cde8fa2b8db
Natasha Umer <natasha.umer2@gmail.com>
2016-06-16 21:17:51 -0400

    added guestbookEntryModel

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,3 +6,8 @@ from django.db import models
 class Guest(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+class GuestbookEntry(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, blank=True)
+    comment = models.CharField(max_length=1023)
